4f8698de69be5633cd1dede92390b798e85dc3d4
t-tomizawa <t-tomizawa@i-enter.co.jp>
2023-04-19 16:47:52 +0900

    Initial commit

---
+++ app/src/main/java/jp/techacademy/takumi/tomizawa/taskapp/Task.kt
@@ -0,0 +1,16 @@
+package jp.techacademy.takumi.tomizawa.taskapp
+
+import java.io.Serializable
+import io.realm.kotlin.types.RealmObject
+import io.realm.kotlin.types.annotations.PrimaryKey
+
+open class Task : RealmObject, Serializable {
+    // idをプライマリーキーとして設定
+    @PrimaryKey
+    var id = 0
+
+    var title = "" // タイトル
+    var contents = "" // 内容
+    var date = "" // 日時
+    var category = "" // カテゴリ
+}
